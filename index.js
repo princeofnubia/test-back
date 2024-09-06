92e4091d75c5c3572c67020091c73ef1655779ef
Oyewo A O <abubakrilaitan@gmail.com>
2024-09-06 04:46:27 -0500

    random_string

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const http = require("http");
-
+const { v4: uuidv4 } = require("uuid");
+const randomString = uuidv4(); // Generates a random UUID
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World\n");
+  res.end(`Hello World ${randomString} \n `);
 });
 
 server.listen(3000, () => {
